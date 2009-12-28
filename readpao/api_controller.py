8860da0d474614c7e7245cb534e24eee115496fc
Cristian Vat <deathy@deathy.info>
2009-12-28 22:01:53 +0200

    Commited changes in response/templates for saving link
    Added template for generation of javascript in response to adding a link
    Simple insertion of db entry from save api

--- readpao/api_controller.py
+++ readpao/api_controller.py
@@ -1,7 +1,13 @@
 import cherrypy
+from readpao.models import ListItem
 
 class APIController(object):
     
     @cherrypy.expose
     def save(self, list=None, url=None):
-       return "Called save with list="+str(list)+" and url="+str(url) 
+        new_item = ListItem(list, url)
+        # TODO: Here we add any other attributes
+        store = cherrypy.thread_data.db_store
+        store.add(new_item)
+        store.commit()
+        return cherrypy.thread_data.templates.get_template("api_save_response.js").render(list_item=new_item) 
---
+++ views/api_save_response.js
@@ -0,0 +1,3 @@
+alert('Just got a reply from readpao saving api!');
+
+alert('You saved item in list ${list_item.list_name} and with url ${list_item.url}');
