1d2f9caa1e9b5575b47053c33113277265e4d257
Cristian Vat <deathy@deathy.info>
2009-12-29 00:34:16 +0200

    Added listing of items with necessary db calls and templates
    added default route in index controller to extract list name and render list view
    added logo and favicon images to static image dirs
    added intro page as index.html

--- readpao/index_controller.py
+++ readpao/index_controller.py
@@ -1,4 +1,5 @@
 import cherrypy
+from readpao.models import ListItem
 
 class IndexController(object):
 
@@ -8,5 +9,11 @@ class IndexController(object):
 
     @cherrypy.expose
     def secretAlfa(self):
-	return cherrypy.thread_data.templates.get_template("secret-alfa.html").render(title="ReadPao - Take-out reading - the secret is not out!")
+	    return cherrypy.thread_data.templates.get_template("secret-alfa.html").render(title="ReadPao - Take-out reading - the secret is not out!")
+
+    @cherrypy.expose
+    def default(*args, **kwargs):
+        store = cherrypy.thread_data.db_store
+        list_items = store.find(ListItem, ListItem.list_name==args[1])
+        return cherrypy.thread_data.templates.get_template("list.html").render(list_items=list_items)
 
