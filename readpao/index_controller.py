61b5dbf338b89bf9345d463db917af81ae839110
Monica Pop <moni@sunshine.(none)>
2009-12-29 05:01:44 +0200

    design list items add item view

--- readpao/index_controller.py
+++ readpao/index_controller.py
@@ -10,10 +10,16 @@ class IndexController(object):
     @cherrypy.expose
     def secretAlfa(self):
 	    return cherrypy.thread_data.templates.get_template("secret-alfa.html").render(title="ReadPao - Take-out reading - the secret is not out!")
+    
+    @cherrypy.expose
+    def read(self,id):
+        store = cherrypy.thread_data.db_store
+        item = store.find(ListItem, ListItem.id == int(id)).one()
+        return cherrypy.thread_data.templates.get_template("item.html").render(item=item)
 
     @cherrypy.expose
     def default(*args, **kwargs):
         store = cherrypy.thread_data.db_store
-        list_items = store.find(ListItem, ListItem.list_name==args[1])
-        return cherrypy.thread_data.templates.get_template("list.html").render(list_items=list_items)
-
+        list_name = args[1]
+        list_items = store.find(ListItem, ListItem.list_name==list_name)
+        return cherrypy.thread_data.templates.get_template("list.html").render(list_items=list_items, list_name=list_name)
