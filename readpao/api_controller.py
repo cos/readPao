cd54a1c086e8c951298054ff583256dac8259eab
Cristian Vat <deathy@deathy.info>
2009-12-29 04:02:12 +0200

    Added content grabber and extended data model with reading information
    Added content grabber ( based on work at http://nirmalpatel.com/fcgi/hn.py ) and information from Arc90 Readability project.
    Added information about reading time of an article into the database.

--- readpao/api_controller.py
+++ readpao/api_controller.py
@@ -1,11 +1,23 @@
 import cherrypy
 from readpao.models import ListItem
+from readpao.content_grabber import *
+import re
+import math
 
 class APIController(object):
     
     @cherrypy.expose
     def save(self, list=None, url=None, rand=None):
+        # TODO: Validate list, url. Check if they are not None and if they don't already exist.
         new_item = ListItem(list, url)
+        link,link_title,link_content,soup = myGrabContent(url)
+        #link_content = re.sub("[\s]+"," ", link_content)
+        soup_content = u''.join(soup.findAll(text=True))
+        soup_content = re.sub("\\s[^\\s]{1,2}\\s"," ",soup_content)
+        soup_content = re.sub("[\\s]+", " ", soup_content)
+        new_item.content = link_content
+        new_item.title = link_title
+        new_item.time_to_read = round(len(soup_content.split(" "))/200.0)
         # TODO: Here we add any other attributes
         store = cherrypy.thread_data.db_store
         store.add(new_item)
